1fa1e1599be76a63930e2911d6d176d12bb53f7f
Sergio Meireles Nibon Junior <snibonjr@gmail.com>
2025-12-20 17:30:40 -0300

    teste

--- DatabaseLibrary-CORRIGIDO/my_sql_scripts/insert_into_account.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO account (user_id, username, password, email)
-VALUES (1, 'May Fernandes', 'robot123', 'robotizando@robot.com')
--- Process/hello_world_fail.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-print("Hello Robotizador!!!")
-print("Teste de execução de processo com erro!!!!")
-sys.exit("FAIL: Erro proposital!!!")
